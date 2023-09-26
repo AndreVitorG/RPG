613e47e5660814b21d7edf3a402e682a6143bac2
André Vitor Gabriel <94085958+AndreVitorG@users.noreply.github.com>
2023-09-26 08:55:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -45,31 +45,33 @@ def populate(matrix):
 				if (k+1) < (len(matrix)):
 					a = matrix[k-1][j]
 					b = matrix[k+1][j]
-#					printMatrix(matrix)
-#					print(" ")
+##					printMatrix(matrix)
+##					print(" ")
 					if b > a:
 						matrix[k].append((random.randint(a, b)))
 					else:
 						matrix[k].append((random.randint(b, a)))
-#				elif k+1 > len(matrix) or k > len(matrix):
-#					pass
+##				elif k+1 > len(matrix) or k > len(matrix):
+##					pass
 		else:
 			pass
 
 
-	#HORIZONTAL  --  funciona apenas na primeira linha
-	for list in matrix:
+    #HORIZONTAL  --  funciona
+	for i in range(len(matrix)-1):
 		aux = 1
-		for j in range(len(list)):
+		for j in range((width)-1):
 			printMatrix(matrix)
 			aux += 1
-			a = list[j+aux-1]
-			b = list[j+aux+1]
+##			print(j+aux-1)
+			a = matrix[i][j+aux-1]
+##			print(a)
+			b = matrix[i][j+aux]
+##			print(b)
 			if b > a:
-				list.insert(j+aux, random.randint(a, b))
+				matrix[i].insert(j+aux, random.randint(a, b))
 			else:
-				list.insert(j+aux, random.randint(b, a))
-
+				matrix[i].insert(j+aux, random.randint(b, a))
 
 	return matrix
 
